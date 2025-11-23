ad41288d2e73f29214ea16fcbde83823d21cb495
Rowan Fortier <rowan.fortier@gmail.com>
2025-11-22 22:01:18 -0800

    make icon change color when connect is highlighted

--- frontend/src/canvas-extension/content.js
+++ frontend/src/canvas-extension/content.js
@@ -42,7 +42,7 @@ function initExtension(globalNav) {
     navItem.innerHTML = `
         <a id="global_nav_clubs_link" href="#" class="ic-app-header__menu-list-link">
             <div class="menu-item-icon-container" aria-hidden="true">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+                <svg class="ic-icon-svg ic-icon-svg--dashboard" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                     <path d="M160-120q-33 0-56.5-23.5T80-200v-560q0-33 23.5-56.5T160-840h560q33 0 56.5 23.5T800-760v80h80v80h-80v80h80v80h-80v80h80v80h-80v80q0 33-23.5 56.5T720-120H160Zm0-80h560v-560H160v560Zm80-80h200v-160H240v160Zm240-280h160v-120H480v120Zm-240 80h200v-200H240v200Zm240 200h160v-240H480v240ZM160-760v560-560Z"/>
                 </svg>
             </div>
@@ -66,9 +66,11 @@ function initExtension(globalNav) {
 
 async function toggleTray() {
     let tray = document.getElementById('ubc-clubs-tray');
+    let menutab = document.getElementById('ubc-clubs-nav-item');
     
     if (tray) {
         tray.classList.toggle('tray-open');
+        menutab.classList.toggle('ic-app-header__menu-list-item--active');
         return;
     }
 
@@ -85,6 +87,7 @@ async function toggleTray() {
         // Close Handler
         document.getElementById('ubc-tray-close').addEventListener('click', () => {
             document.getElementById('ubc-clubs-tray').classList.remove('tray-open');
+            menutab.classList.toggle('ic-app-header__menu-list-item--active');
         });
 
         // VIEW LOGIC
